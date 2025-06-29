3fd19a09c5658b512946a5e16be32e0ffd8f8766
Irham Sahbana <irhamsahbana@gmail.com>
2025-06-29 15:06:47 +0700

    refactor: (report) include allocated_at in UpdateRegistrationsPaidAt query for accurate updates

--- internal/module/report/entity/registration_paid_at.go
+++ internal/module/report/entity/registration_paid_at.go
@@ -4,5 +4,6 @@ type UpdateRegisPaidAtReq struct {
 	UserId string `json:"user_id" validate:"required,ulid"`
 
 	PaidAt          string   `json:"paid_at" validate:"required,datetime=2006-01-02"`
+	AllocatedAt     string   `json:"allocated_at" validate:"required,datetime=2006-01-02"`
 	RegistrationIds []string `json:"registration_ids" validate:"required,min=1,dive,ulid"`
 }
--- internal/module/report/repository/registration_paid_at.go
+++ internal/module/report/repository/registration_paid_at.go
@@ -20,6 +20,7 @@ func (r *reportRepo) UpdateRegistrationsPaidAt(ctx context.Context, req *entity.
 		UPDATE program_registrations
 		SET
 			paid_at = ($1::timestamp AT TIME ZONE 'Asia/Makassar'),
+			allocated_at = ($2::timestamp AT TIME ZONE 'Asia/Makassar'),
 			is_paid = TRUE,
 			updated_at = NOW()
 		WHERE id = $2
@@ -27,7 +28,10 @@ func (r *reportRepo) UpdateRegistrationsPaidAt(ctx context.Context, req *entity.
 	`
 
 	for _, registrationId := range req.RegistrationIds {
-		if _, err := tx.ExecContext(ctx, query, (req.PaidAt + " 01:00:00"), registrationId); err != nil {
+		if _, err := tx.ExecContext(ctx, query,
+			(req.PaidAt + " 01:00:00"),
+			(req.AllocatedAt + " 01:00:00"),
+			registrationId); err != nil {
 			log.Error().Err(err).Any("req", req).Msgf("%s - failed to update registration paid_at for id %s", fnName, registrationId)
 			return err
 		}
