1f83cf011d810cd60a7db474c9cfbcab0b8528ef
Irham Sahbana <irhamsahbana@gmail.com>
2025-09-28 16:49:35 +0700

    feat(report): add program_acquisition_rights field to report entities
    Add new field to template and registration report responses to track program acquisition rights

--- internal/module/report/entity/registration_detail.go
+++ internal/module/report/entity/registration_detail.go
@@ -9,39 +9,40 @@ type GetRegistrationReq struct {
 }
 
 type GetRegistrationResp struct {
-	ID                    string        `json:"id" db:"id"`
-	ProgramID             string        `json:"program_id" db:"program_id"`
-	MarketerID            string        `json:"marketer_id" db:"marketer_id"`
-	LecturerID            *string       `json:"lecturer_id" db:"lecturer_id"`
-	StudentID             string        `json:"student_id" db:"student_id"`
-	StudentManagerID      string        `json:"student_manager_id" db:"student_manager_id"`
-	StudentIdentifier     string        `json:"student_identifier" db:"student_identifier"`
-	ProgramName           string        `json:"program_name" db:"program_name"`
-	ProgramFee            float64       `json:"program_fee" db:"program_fee"`
-	LecturerName          *string       `json:"lecturer_name" db:"lecturer_name"`
-	StudentManagerName    string        `json:"student_manager_name" db:"student_manager_name"`
-	MarketerName          string        `json:"marketer_name" db:"marketer_name"`
-	StudentName           string        `json:"student_name" db:"student_name"`
-	AdministrationFee     *float64      `json:"administration_fee" db:"administration_fee"`
-	FLFee                 *float64      `json:"foreign_learning_fee" db:"foreign_learning_fee"`
-	NLFee                 *float64      `json:"night_learning_fee" db:"night_learning_fee"`
-	IsITP                 bool          `json:"is_itp" db:"is_itp"`
-	MarketerCommissionFee float64       `json:"marketer_commission_fee" db:"marketer_commission_fee"`
-	OverpaymentFee        *float64      `json:"overpayment_fee" db:"overpayment_fee"`
-	HRFee                 float64       `json:"hr_fee" db:"hr_fee"`
-	HRFeeForMentor        *float64      `json:"hr_fee_for_mentor" db:"hr_fee_for_mentor"`
-	HRFeeForHR            *float64      `json:"hr_fee_for_hr" db:"hr_fee_for_hr"`
-	MarketerGiftsFee      float64       `json:"marketer_gifts_fee" db:"marketer_gifts_fee"`
-	ClosingFeeForOffice   *float64      `json:"closing_fee_for_office" db:"closing_fee_for_office"`
-	ClosingFeeForReward   *float64      `json:"closing_fee_for_reward" db:"closing_fee_for_reward"`
-	Profit                float64       `json:"profit" db:"profit"`
-	Notes                 *string       `json:"notes" db:"notes"`
-	Batch                 *string       `json:"batch" db:"batch"`
-	IsPaid                bool          `json:"is_paid" db:"is_paid"`
-	Students              []AddStudent  `json:"additional_students"`
-	Days                  pq.Int64Array `json:"days" db:"days"`
-	CreatedAt             string        `json:"created_at" db:"created_at"`
-	UpdatedAt             string        `json:"updated_at" db:"updated_at"`
-	PaidAt                *string       `json:"paid_at" db:"paid_at"`
-	AllocatedAt           *string       `json:"allocated_at" db:"allocated_at"`
+	ID                       string        `json:"id" db:"id"`
+	ProgramID                string        `json:"program_id" db:"program_id"`
+	MarketerID               string        `json:"marketer_id" db:"marketer_id"`
+	LecturerID               *string       `json:"lecturer_id" db:"lecturer_id"`
+	StudentID                string        `json:"student_id" db:"student_id"`
+	StudentManagerID         string        `json:"student_manager_id" db:"student_manager_id"`
+	StudentIdentifier        string        `json:"student_identifier" db:"student_identifier"`
+	ProgramName              string        `json:"program_name" db:"program_name"`
+	ProgramFee               float64       `json:"program_fee" db:"program_fee"`
+	LecturerName             *string       `json:"lecturer_name" db:"lecturer_name"`
+	StudentManagerName       string        `json:"student_manager_name" db:"student_manager_name"`
+	MarketerName             string        `json:"marketer_name" db:"marketer_name"`
+	StudentName              string        `json:"student_name" db:"student_name"`
+	AdministrationFee        *float64      `json:"administration_fee" db:"administration_fee"`
+	FLFee                    *float64      `json:"foreign_learning_fee" db:"foreign_learning_fee"`
+	NLFee                    *float64      `json:"night_learning_fee" db:"night_learning_fee"`
+	IsITP                    bool          `json:"is_itp" db:"is_itp"`
+	MarketerCommissionFee    float64       `json:"marketer_commission_fee" db:"marketer_commission_fee"`
+	OverpaymentFee           *float64      `json:"overpayment_fee" db:"overpayment_fee"`
+	HRFee                    float64       `json:"hr_fee" db:"hr_fee"`
+	HRFeeForMentor           *float64      `json:"hr_fee_for_mentor" db:"hr_fee_for_mentor"`
+	HRFeeForHR               *float64      `json:"hr_fee_for_hr" db:"hr_fee_for_hr"`
+	MarketerGiftsFee         float64       `json:"marketer_gifts_fee" db:"marketer_gifts_fee"`
+	ClosingFeeForOffice      *float64      `json:"closing_fee_for_office" db:"closing_fee_for_office"`
+	ClosingFeeForReward      *float64      `json:"closing_fee_for_reward" db:"closing_fee_for_reward"`
+	Profit                   float64       `json:"profit" db:"profit"`
+	Notes                    *string       `json:"notes" db:"notes"`
+	Batch                    *string       `json:"batch" db:"batch"`
+	IsPaid                   bool          `json:"is_paid" db:"is_paid"`
+	Students                 []AddStudent  `json:"additional_students"`
+	ProgramAcquisitionRights int           `json:"program_acquisition_rights" db:"program_acquisition_rights"`
+	Days                     pq.Int64Array `json:"days" db:"days"`
+	CreatedAt                string        `json:"created_at" db:"created_at"`
+	UpdatedAt                string        `json:"updated_at" db:"updated_at"`
+	PaidAt                   *string       `json:"paid_at" db:"paid_at"`
+	AllocatedAt              *string       `json:"allocated_at" db:"allocated_at"`
 }
