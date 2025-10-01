083891ee9d5eaec1b52e1cfec0a3307384cc44f9
Irham Sahbana <irhamsahbana@gmail.com>
2025-10-02 00:07:53 +0700

    refactor(report): simplify conditional logic in SQL query
    Remove redundant CASE statement and always calculate the same expression for the hr_detail_fee calculation

--- internal/module/report/repository/registration_multi_allocation.go
+++ internal/module/report/repository/registration_multi_allocation.go
@@ -215,16 +215,10 @@ SELECT
 		- (40000 * CASE WHEN (SELECT is_itp FROM template) THEN 2 ELSE 1 END * (SELECT acquisition_rights FROM template))
 	),
 	NULL,
-	CASE
-		WHEN
-		? >= TO_CHAR(NOW() AT TIME ZONE 'Asia/Makassar', 'YYYY-MM')
-		THEN NULL
-		ELSE
-			(
-				(SELECT hr_fee FROM template)
-				- (40000 * CASE WHEN (SELECT is_itp FROM template) THEN 2 ELSE 1 END * (SELECT acquisition_rights FROM template))
-			)
-	END,
+	(
+		(SELECT hr_fee FROM template)
+		- (40000 * CASE WHEN (SELECT is_itp FROM template) THEN 2 ELSE 1 END * (SELECT acquisition_rights FROM template))
+	),
 	(SELECT marketer_gifts_fee FROM template),
 	(SELECT closing_fee_for_office FROM template),
 	(SELECT closing_fee_for_reward FROM template),
