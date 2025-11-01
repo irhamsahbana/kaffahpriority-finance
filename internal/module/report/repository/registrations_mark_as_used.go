8aabc0e34315f3958b64b254477b6126dd6f254d
Irham Sahbana <irhamsahbana@gmail.com>
2025-11-01 17:01:56 +0700

    refactor(repository): simplify SQL query timestamp formatting
    Use more concise timestamp formatting in the SQL query while maintaining the same functionality

--- internal/module/report/repository/registrations_mark_as_used.go
+++ internal/module/report/repository/registrations_mark_as_used.go
@@ -10,20 +10,16 @@ import (
 func (r *reportRepo) RegistrationsMarkAsUsed(ctx context.Context, req *entity.RegistrationsMarkAsUsedReq) error {
 	fnName := "repo::RegistrationsMarkAsUsed"
 
-	query := `
+    query := `
         UPDATE program_registrations
             SET mentor_detail_fee_used = mentor_detail_fee
         WHERE
             deleted_at IS NULL
-			AND
-			mentor_detail_fee_used IS NULL
-			AND
-			category = 'general'
-			AND
-			program_meetings > 0
-            AND
-            allocated_at >= (TO_TIMESTAMP(?, 'YYYY-MM') AT TIME ZONE 'Asia/Makassar')
-            AND allocated_at < ((TO_TIMESTAMP(?, 'YYYY-MM') AT TIME ZONE 'Asia/Makassar') + INTERVAL '1 month')
+			AND mentor_detail_fee_used IS NULL
+			AND category = 'general'
+			AND program_meetings > 0
+            AND allocated_at >= ((? || '-01 00:00:00+08')::timestamptz)
+            AND allocated_at < (((? || '-01 00:00:00+08')::timestamptz) + INTERVAL '1 month')
     `
 
 	if _, err := r.db.ExecContext(ctx, r.db.Rebind(query), req.AllocatedMonth, req.AllocatedMonth); err != nil {
