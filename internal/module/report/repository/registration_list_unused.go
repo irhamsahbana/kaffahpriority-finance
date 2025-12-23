fffeb8b1ad3eb3c58151b233dc129b7a98562990
rizkyalfiansyah <rizkkyaf@gmail.com>
2025-12-24 00:35:04 +0800

    fix: change status to full if hr_fee is 0 on unused registration api

--- internal/module/report/repository/registration_list_unused.go
+++ internal/module/report/repository/registration_list_unused.go
@@ -51,6 +51,7 @@ func (r *reportRepo) GetUnusedRegistrations(ctx context.Context, req *entity.Get
 			pr.mentor_detail_fee - pr.mentor_detail_fee_used AS hr_fee_for_mentor_remaining,
 			CASE
 				WHEN pr.mentor_detail_fee_used = pr.mentor_detail_fee THEN 'full'
+				WHEN pr.hr_fee = 0 THEN 'full'
 				WHEN pr.mentor_detail_fee_used IS NULL THEN NULL
 				ELSE 'partial'
 			END AS hr_fee_for_mentor_status,
